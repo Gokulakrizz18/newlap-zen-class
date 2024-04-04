a3b36c13794b12c55d7145a3d60d391d44f88c0f
Gokulakrizz18 <gokul02.ravi@gmail.com>
2024-04-04 10:24:46 +0530

    async operation

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-var request = new XMLHttpRequest();
-request.open("GET","https://restcountries.com/v3.1/all")
-request.send();
-request.onload = function(){
-    var res = JSON.parse(request.response)
-    console.log(res);
-    for(var i=0;i<res.length;i++){
-        if(res[i].capital){
-            console.log(res[i].capital[0]+" "+res[i].area)
-        }else
-        {
-            console.log("INVALID CAPITAL")
-        }
-    }
-    
-
-}
+console.log("hello world")
+setTimeout(()=>console.log("Hi hello welcome"),2000)
+console.log("he")
