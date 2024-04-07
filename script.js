ba5ac68145c21306512af82312997e7ca82de97f
Gokulakrizz18 <gokul02.ravi@gmail.com>
2024-04-07 23:22:17 +0530

    promise

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-console.log("hello world")
-setTimeout(()=>console.log("Hi hello welcome"),2000)
-console.log("he")
+var p1 = new Promise((resolve,reject)=>{
+    var age = parseInt(prompt("Enter the name of the person"))
+    if(age >= 18){
+        return resolve("You are eligible")
+
+    }
+    else{
+        return reject("Poda Punda")
+    }
+})
+
+p1.then((data)=>console.log(data)).catch((error)=>console.log(error))
