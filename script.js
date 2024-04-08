306a08e4516f664112afc86574ff83d0364fac9d
Gokulakrizz18 <gokul02.ravi@gmail.com>
2024-04-08 23:09:10 +0530

    async and await

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-var p1 = new Promise((resolve,reject)=>{
-    var age = parseInt(prompt("Enter the name of the person"))
-    if(age >= 18){
-        return resolve("You are eligible")
-
-    }
-    else{
-        return reject("Poda Punda")
-    }
-})
-
-p1.then((data)=>console.log(data)).catch((error)=>console.log(error))
+async function foo(){
+    var res = await fetch("https://restcountries.com/v3.1/all")
+    var res1 = await res.json()
+    console.log(res1)
+}
